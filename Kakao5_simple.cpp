99e7694426422d5dfa187d315e91a3961d78d61c
EadCat <eadyoung@naver.com>
2020-05-02 17:48:07 +0900

    Kakao5 internship prob. No.5 (Binary Search Not applied)

---
+++ Kakao5_simple.cpp
@@ -0,0 +1,24 @@
+//#include <vector>
+//
+//using namespace std;
+//
+//#define MAX_COND 200000000
+//
+//int solution(vector<int> stones, int k) {
+//    int answer;
+//    int tries = stones.size() - k + 1;
+//    int z = 0;
+//    answer = MAX_COND;
+//
+//    for (int i = 0; i < tries; i++) {
+//        for (int j = 0; j < k; j++) {
+//            if (z < stones[i + j])
+//                z = stones[i + j];
+//        }
+//        if (answer > z)
+//            answer = z;
+//        z = 0;
+//    }
+//
+//    return answer;
+//}
